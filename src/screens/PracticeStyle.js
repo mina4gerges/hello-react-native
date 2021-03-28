0376fde22442f71a76b23d58bc4dcf8f0bbde77b
mina gerges <minagerges4@gmail.com>
2021-03-28 11:24:16 +0300

    learn how to use Alert native component

--- src/screens/PracticeStyle.js
+++ src/screens/PracticeStyle.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, TouchableWithoutFeedback, Alert, Keyboard, StyleSheet} from 'react-native';
 import TextInputComp from "../components/TextInputComp";
 import ButtonComp from "../components/ButtonComp";
 
 const PracticeStyle = () => {
 
     const onButtonPress = () => {
+        Alert.alert(
+            "Hello I am title",
+            "Me, I am a message",
+            [{
+                text: 'Ok',
+                style: 'destructive',
+                onPress: () => console.log("I pressed Ok")
+            }, {
+                text: 'Cancel',
+                style: 'cancel',
+                onPress: () => console.log("I pressed cancel")
+            }])
     };
 
     return (
-        <View style={styles.startGameScreen}>
-            <Text style={styles.text}>
-                Hello from StartGameScreen
-            </Text>
-            <TextInputComp placeholder='Type your name here'/>
-            <ButtonComp label='Click me' onButtonPress={onButtonPress}/>
-        </View>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View style={styles.startGameScreen}>
+                <Text style={styles.text}>
+                    Hello from StartGameScreen
+                </Text>
+                <TextInputComp placeholder='Type your name here'/>
+                <ButtonComp label='Show Alert' onButtonPress={onButtonPress}/>
+            </View>
+        </TouchableWithoutFeedback>
     )
 
 }
