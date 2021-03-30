de5b93843300c06bf5af27b2e0d92610e12726c7
mina gerges <minagerges4@gmail.com>
2021-03-30 10:33:41 +0300

    use Platform.OS, Platform.select({ios:{},android:{}}), Platform.Version

--- src/components/ButtonComp.js
+++ src/components/ButtonComp.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {TouchableOpacity, Text, Dimensions, StyleSheet} from "react-native";
+import {TouchableOpacity, Text, Platform, Dimensions, StyleSheet} from "react-native";
 import colors from "../constants/colors";
 
 const ButtonComp = ({label, onButtonPress}) => {
@@ -14,8 +14,16 @@ const ButtonComp = ({label, onButtonPress}) => {
         return () => Dimensions.removeEventListener('change', updateWidth);
     })
 
+    //! N.B: we can use:
+    //! Platform.OS === 'android' || Platform.OS === 'ios'
+    //! Platform.Version >= 21 (for ex)
+
     return (
-        <TouchableOpacity style={{...styles.button, width}} activeOpacity={0.8} onPress={onButtonPress}>
+        <TouchableOpacity style={{
+            ...styles.buttonBase,
+            ...Platform.select({android: styles.buttonAndroid, ios: styles.buttonIOS}),
+            width
+        }} activeOpacity={0.8} onPress={onButtonPress}>
             <Text>
                 {label}
             </Text>
@@ -24,14 +32,13 @@ const ButtonComp = ({label, onButtonPress}) => {
 }
 
 const styles = StyleSheet.create({
-    button: {
+    buttonBase: {
         margin: 5,
         borderWidth: 1,
         borderRadius: 5,
         paddingVertical: 3,
         paddingHorizontal: 5,
         borderColor: 'black',
-        backgroundColor: 'white',
 
         // Only property for IOS
         shadowRadius: 8,
@@ -41,6 +48,12 @@ const styles = StyleSheet.create({
 
         // Only property for Android:
         elevation: 5,
+    },
+    buttonIOS: {
+        backgroundColor: 'grey',
+    },
+    buttonAndroid: {
+        backgroundColor: 'white',
     }
 })
 
